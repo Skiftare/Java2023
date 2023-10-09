bb39ed67d30c6cda2d1945eda00f3c6db752bc54
skiftare <74261401+Skiftare@users.noreply.github.com>
2023-10-09 22:40:07 +0500

    Update Task8.java
    убрал magicNumber

--- src/main/java/edu/hw1/Task8.java
+++ src/main/java/edu/hw1/Task8.java
@@ -1,16 +1,19 @@
 package edu.hw1;
 
 import java.util.ArrayList;
+import org.jetbrains.annotations.NotNull;
 
 @SuppressWarnings("HideUtilityClassConstructor")
 
 public class Task8 {
-    @SuppressWarnings("MagicNumber")
-    public static boolean knightBoardCapture(int[][] board) {
+    private static final Integer NEED_D = 5;
+    private static final Integer HORSE = 1;
+
+    public static boolean knightBoardCapture(int[] @NotNull [] board) {
         ArrayList<int[]> knightCoords = new ArrayList<>();
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
-                if (board[i][j] == 1) {
+                if (board[i][j] == HORSE) {
                     knightCoords.add(new int[] {i, j});
                 }
             }
@@ -24,7 +27,7 @@ public class Task8 {
                 int deltaY = coordsTwo[1] - coordsOne[1];
                 int resDistance = (int) Math.pow(deltaX, 2) + (int) Math.pow(deltaY, 2);
 
-                if (resDistance == 5) {
+                if (resDistance == NEED_D) {
                     ans = false;
                     break;
                 }
