4c06b1c9d0e669f61ac6c31096560c91360397ad
skiftare <74261401+Skiftare@users.noreply.github.com>
2023-10-09 21:43:07 +0500

    Update Task7Test.java
    camelCase

--- src/test/java/edu/hw1/Task7Test.java
+++ src/test/java/edu/hw1/Task7Test.java
@@ -7,14 +7,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Task7Test
 {
     @Test
-    void standart_test_right() {
+    void standartTestRight() {
 
         assertEquals(4, Task7.rotateRight(8, 1));
         assertEquals(115, Task7.rotateRight(155,3));//10011 011 -> 1 10011 01 -> 1 1 10011 0->01110011
 
     }
     @Test
-    void standart_test_left(){
+    void standartTestLeft(){
         assertEquals(1, Task7.rotateLeft(16, 1));
         assertEquals(6, Task7.rotateLeft(17, 2));
         assertEquals(142,Task7.rotateLeft(456, 4));//1110 01000 -> 01000 1110
