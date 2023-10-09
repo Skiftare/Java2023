0a413679e630e063cfcec96dcb1326824c767114
skiftare <74261401+Skiftare@users.noreply.github.com>
2023-10-09 21:40:56 +0500

    Update Task5Test.java

--- src/test/java/edu/hw1/Task5Test.java
+++ src/test/java/edu/hw1/Task5Test.java
@@ -1,28 +1,29 @@
 package edu.hw1;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Task5Test {
     @Test
     void standart() {
-        assertEquals(true, Task5.isPalindromeDescendant(11211230));
-        assertEquals(true, Task5.isPalindromeDescendant(13001120));
-        assertEquals(true, Task5.isPalindromeDescendant(23336014));
-        assertEquals(true, Task5.isPalindromeDescendant(11));
+        assertTrue(Task5.isPalindromeDescendant(11211230));
+        assertTrue(Task5.isPalindromeDescendant(13001120));
+        assertTrue(Task5.isPalindromeDescendant(23336014));
+        assertTrue(Task5.isPalindromeDescendant(11));
     }
 
     @Test
-    void odd_length_of_num() {
-        assertEquals(true, Task5.isPalindromeDescendant(121));
-        assertEquals(false, Task5.isPalindromeDescendant(123));
+    void oddLengthOfNum() {
+        assertTrue(Task5.isPalindromeDescendant(121));
+        assertFalse(Task5.isPalindromeDescendant(123));
     }
 
     @Test
-    void sum_of_two_numbers_more_10() {
+    void sumNumbersMore10() {
 
-        assertEquals(true, Task5.isPalindromeDescendant(5683));
-        assertEquals(true, Task5.isPalindromeDescendant(5582));//1010 -> 11
+        assertTrue(Task5.isPalindromeDescendant(5683));
+        assertTrue(Task5.isPalindromeDescendant(5582));//1010 -> 11
     }
 
 }
