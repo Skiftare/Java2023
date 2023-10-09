03fbe4082ad04ac8cbe1b98c00cc7d442bdcd996
skiftare <74261401+Skiftare@users.noreply.github.com>
2023-10-09 23:22:43 +0500

    Update Task2.java
    Притащил delLast из Task5

--- src/main/java/edu/hw1/Task2.java
+++ src/main/java/edu/hw1/Task2.java
@@ -1,9 +1,9 @@
 package edu.hw1;
 
-@SuppressWarnings("HideUtilityClassConstructor")
+import static edu.hw1.Task5.delLast;
 
+@SuppressWarnings("HideUtilityClassConstructor")
 public class Task2 {
-    private static final int TEN_N = 10;
 
     public static int countDigits(int n) {
         if (n == 0) {
@@ -12,8 +12,8 @@ public class Task2 {
         int secN = Math.abs(n);
         int cnt = 0;
         while (secN > 0) {
-            cnt += 1;
-            secN /= TEN_N;
+            secN = delLast(secN);
+            cnt++;
         }
         return cnt;
     }
