1790d7bea60959e8bbd62a4cd50ea08681a1faf5
skiftare <74261401+Skiftare@users.noreply.github.com>
2023-10-09 21:42:41 +0500

    Update Task6Test.java
    camelCase

--- src/test/java/edu/hw1/Task6Test.java
+++ src/test/java/edu/hw1/Task6Test.java
@@ -5,13 +5,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Task6Test {
     @Test
-    void Standarttest(){
+    void standartTest(){
         assertEquals(5, Task6.funK(6621));
         assertEquals(4, Task6.funK(6554));
         assertEquals(3, Task6.funK(1234));
     }
     @Test
-    void not_four_digit(){
+    void notFourDigit(){
         assertEquals(-1, Task6.funK(0000));
         assertEquals(-1, Task6.funK(1000));
     }
