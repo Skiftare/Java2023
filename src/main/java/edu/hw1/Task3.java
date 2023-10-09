3117af61f3ccaff7f750830190fb163bdbdb53e3
skiftare <74261401+Skiftare@users.noreply.github.com>
2023-10-09 21:34:31 +0500

    Update Task3.java
    Перенес инициализацию минимума-максимума, учёл пустые массивы

--- src/main/java/edu/hw1/Task3.java
+++ src/main/java/edu/hw1/Task3.java
@@ -5,30 +5,24 @@ import org.jetbrains.annotations.NotNull;
 @SuppressWarnings("HideUtilityClassConstructor")
 public class Task3 {
     public static boolean isNestable(int @NotNull [] fstMas, int[] secMas) {
-
-        int minFst = 0;
-        int maxFst = 0;
-        int minSec = 0;
-        int maxSec = 0;
-
-        for (int i = 0; i < fstMas.length; i++) {
-            if (i == 0) {
-                minFst = fstMas[i];
-                maxFst = fstMas[i];
-            } else {
-                minFst = Math.min(minFst, fstMas[i]);
-                maxFst = Math.max(maxFst, fstMas[i]);
-            }
-
+        if (fstMas.length == 0 && secMas.length != 0) {
+            return true;
         }
-        for (int i = 0; i < secMas.length; i++) {
-            if (i == 0) {
-                minSec = secMas[i];
-                maxSec = secMas[i];
-            } else {
-                minSec = Math.min(minSec, secMas[i]);
-                maxSec = Math.max(maxSec, secMas[i]);
-            }
+        if (secMas.length == 0) {
+            return false;
+        }
+        int minFst = fstMas[0];
+        int maxFst = fstMas[0];
+        int minSec = secMas[0];
+        int maxSec = secMas[0];
+
+        for (int i = 1; i < fstMas.length; i++) {
+            minFst = Math.min(minFst, fstMas[i]);
+            maxFst = Math.max(maxFst, fstMas[i]);
+        }
+        for (int i = 1; i < secMas.length; i++) {
+            minSec = Math.min(minSec, secMas[i]);
+            maxSec = Math.max(maxSec, secMas[i]);
         }
 
         return (minFst > minSec && maxFst < maxSec);
