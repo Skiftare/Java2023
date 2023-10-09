34acc5f8cdf68020342f26fcd07700eff845dca1
skiftare <74261401+Skiftare@users.noreply.github.com>
2023-10-09 22:43:45 +0500

    Update Task4.java
    Сделал больше буковок в переменных

--- src/main/java/edu/hw1/Task4.java
+++ src/main/java/edu/hw1/Task4.java
@@ -4,15 +4,15 @@ import org.jetbrains.annotations.NotNull;
 
 @SuppressWarnings("HideUtilityClassConstructor")
 public class Task4 {
-    public static @NotNull String fixString(@NotNull String s) {
-        StringBuilder res = new StringBuilder();
-        for (int i = 0; i < s.length() - (s.length() % 2); i += 2) {
-            res.append(s.charAt(i + 1));
-            res.append(s.charAt(i));
+    public static @NotNull String fixString(@NotNull String brokenString) {
+        StringBuilder fixedString = new StringBuilder();
+        for (int i = 0; i < brokenString.length() - (brokenString.length() % 2); i += 2) {
+            fixedString.append(brokenString.charAt(i + 1));
+            fixedString.append(brokenString.charAt(i));
         }
-        if (s.length() % 2 == 1) {
-            res.append(s.charAt(s.length() - 1));
+        if (brokenString.length() % 2 == 1) {
+            fixedString.append(brokenString.charAt(brokenString.length() - 1));
         }
-        return res.toString();
+        return fixedString.toString();
     }
 }
