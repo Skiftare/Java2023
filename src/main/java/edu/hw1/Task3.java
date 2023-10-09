8000a088d780fcb443c520126979dc13537e7773
skiftare <74261401+Skiftare@users.noreply.github.com>
2023-10-09 22:58:43 +0500

    Update Task3.java
    Решил не изобретать велосипед и упростить. Добавил notNull, чтобы брать 0-элемент, сделал сортировку. Да, работает медленнее, зато читать приятнее.

--- src/main/java/edu/hw1/Task3.java
+++ src/main/java/edu/hw1/Task3.java
@@ -1,32 +1,14 @@
 package edu.hw1;
 
+import java.util.Arrays;
 import org.jetbrains.annotations.NotNull;
+
 
 @SuppressWarnings("HideUtilityClassConstructor")
 public class Task3 {
     public static boolean isNestable(int @NotNull [] fstMas, int[] secMas) {
-        if (fstMas.length == 0 && secMas.length != 0) {
-            return true;
-        }
-
-        if (secMas.length == 0) {
-            return false;
-        }
-
-        int minFst = fstMas[0];
-        int maxFst = fstMas[0];
-        int minSec = secMas[0];
-        int maxSec = secMas[0];
-
-        for (int i = 1; i < fstMas.length; i++) {
-            minFst = Math.min(minFst, fstMas[i]);
-            maxFst = Math.max(maxFst, fstMas[i]);
-        }
-        for (int i = 1; i < secMas.length; i++) {
-            minSec = Math.min(minSec, secMas[i]);
-            maxSec = Math.max(maxSec, secMas[i]);
-        }
-
-        return (minFst > minSec && maxFst < maxSec);
+        Arrays.sort(fstMas);
+        Arrays.sort(secMas);
+        return (fstMas[0] > secMas[0] && fstMas[fstMas.length - 1] < secMas[secMas.length - 1]);
     }
 }
