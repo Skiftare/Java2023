8a98d0a7f72bdc46237a69478d89ac02da3a59ce
skiftare <74261401+Skiftare@users.noreply.github.com>
2023-10-10 21:53:39 +0500

    Update Task1Test.java
    Пояснения к тестам

--- src/test/java/edu/hw1/Task1Test.java
+++ src/test/java/edu/hw1/Task1Test.java
@@ -6,38 +6,42 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Task1Test {
 
-    @DisplayName("Minutes_test")
-
+    @DisplayName("zeroZero")
     @Test
     void zero_zero() {
         assertEquals(0, Task1.minutesToSeconds("00:00"));
 
     }
 
+    @DisplayName("standartTest-10:45")
     @Test
     void standart() {
         assertEquals(10 * 60 + 45, Task1.minutesToSeconds("10:45"));
         assertEquals(10 * 60 + 45, Task1.minutesToSeconds(" 1   0: 4  5 "));
     }
 
+    @DisplayName("Некорректное количество секунд")
     @Test
     void a_lot_secs() {
         assertEquals(-1, Task1.minutesToSeconds("00:61"));
     }
 
+    @DisplayName("Корректное количество секунд, но есть ведущие нули")
     @Test
     void not_a_lot_secs() {
         assertEquals(10, Task1.minutesToSeconds("00:00000000000010"));
         assertEquals(10*60+10, Task1.minutesToSeconds("10:00000000000010"));
     }
 
+    @DisplayName("Минуты/Часы становятся отрицательными")
     @Test
     void minuses() {
         assertEquals(-1, Task1.minutesToSeconds("-10:10"));
         assertEquals(-1, Task1.minutesToSeconds("10:-10"));
         assertEquals(-1, Task1.minutesToSeconds("-10:-10"));
     }
-
+    
+    @DisplayName("Нарушения ввода (буквы, разделители, отсутствуют минуты")
     @Test
     void wrongFormat() {
         assertEquals(-1, Task1.minutesToSeconds("10.10"));
