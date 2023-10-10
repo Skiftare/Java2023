ab502b4ba962ecc052cc76a2bdeb6f3261a9f893
skiftare <74261401+Skiftare@users.noreply.github.com>
2023-10-10 22:04:08 +0500

    Update Task8Test.java

--- src/test/java/edu/hw1/Task8Test.java
+++ src/test/java/edu/hw1/Task8Test.java
@@ -1,9 +1,11 @@
 package edu.hw1;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Task8Test {
+    @DisplayName("1-ый тест из условия. Мир")
     @Test
     void testFirst(){
         int [][] board = {{0, 0, 0, 1, 0, 0, 0, 0},
@@ -16,6 +18,7 @@ public class Task8Test {
             {0, 0, 0, 0, 1, 0, 0, 0}};
         assertTrue(Task8.knightBoardCapture(board));
     }
+    @DisplayName("2-ой тест из условия. Конфликт")
     @Test
     void testSecond(){
         int[][] board = {
@@ -30,6 +33,7 @@ public class Task8Test {
         };
         assertFalse(Task8.knightBoardCapture(board));
     }
+    @DisplayName("3-ий тест из условия. Конфликт")
     @Test
     void testThird() {
         int[][] board = {
