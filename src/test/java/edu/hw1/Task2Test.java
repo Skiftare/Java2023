0403069aaefb819cbd27ddcbec119347b46e4212
skiftare <74261401+Skiftare@users.noreply.github.com>
2023-10-10 13:35:19 +0500

    Update Task2Test.java

--- src/test/java/edu/hw1/Task2Test.java
+++ src/test/java/edu/hw1/Task2Test.java
@@ -5,18 +5,26 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Task2Test {
-    @DisplayName("Проверка длины чисел")
+    @DisplayName("Натуральные числа")
     @Test
     void standart() {
         assertEquals(3, Task2.countDigits(123));
+        assertEquals(1, Task2.countDigits(1));
+        assertEquals(5, Task2.countDigits(12305));
     }
+    
+    @DisplayName("Случай с 0")
     @Test
     void zero() {
         assertEquals(0, Task2.countDigits(0));
         assertEquals(0, Task2.countDigits(-0));
     }
+
+    @DisplayName("Отрицательные числа")
     @Test
     void minus() {
         assertEquals(2, Task2.countDigits(-24));
+        assertEquals(4, Task2.countDigits(-2454));
+        assertEquals(7, Task2.countDigits(-2478908));
     }
 }
