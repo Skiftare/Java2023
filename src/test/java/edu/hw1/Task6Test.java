0f3037514c63a0834139fe83db99561d71f4bc31
skiftare <74261401+Skiftare@users.noreply.github.com>
2023-10-10 22:01:49 +0500

    Update Task6Test.java

--- src/test/java/edu/hw1/Task6Test.java
+++ src/test/java/edu/hw1/Task6Test.java
@@ -1,15 +1,20 @@
 package edu.hw1;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Task6Test {
+    
+    @DisplayName("Стандартная серия тестов")
     @Test
     void standartTest(){
         assertEquals(5, Task6.funK(6621));
         assertEquals(4, Task6.funK(6554));
         assertEquals(3, Task6.funK(1234));
     }
+
+    @DisplayName("Некорректный ввод")
     @Test
     void notFourDigit(){
         assertEquals(-1, Task6.funK(0));
