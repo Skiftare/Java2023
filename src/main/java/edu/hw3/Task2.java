bcc489b6620972002ac07a997c6fd8b383969ae9
skiftare <74261401+Skiftare@users.noreply.github.com>
2023-10-29 11:58:02 +0500

    дз3

--- src/main/java/edu/hw3/Task2.java
+++ src/main/java/edu/hw3/Task2.java
@@ -10,13 +10,13 @@ public class Task2 {
 
     static ArrayList<String> clusterize(@NotNull String message) {
         ArrayList<String> res = new ArrayList<>();
-        char currentChar = ' ';
-        String addStr = "";
+        char currentChar;
+        StringBuilder addStr = new StringBuilder();
         int opened = 0;
         RuntimeException unbalanceException = new RuntimeException("unbalanced string");
         for (int i = 0; i < message.length(); i++) {
             currentChar = message.charAt(i);
-            addStr += currentChar;
+            addStr.append(currentChar);
             if (currentChar == '(') {
                 opened++;
             } else if (currentChar == ')') {
@@ -25,8 +25,8 @@ public class Task2 {
                 throw new RuntimeException("wrong symbol");
             }
             if (opened == 0) {
-                res.add(addStr);
-                addStr = "";
+                res.add(addStr.toString());
+                addStr = new StringBuilder();
             } else if (opened < 0) {
                 throw unbalanceException;
             }
