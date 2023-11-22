dcc3e73f3590bdb67f9756ee44a145e17f658ef8
skiftare <74261401+Skiftare@users.noreply.github.com>
2023-11-22 21:39:23 +0500

    дз 1 есть

---
+++ src/main/java/edu/hw7/Task1.java
@@ -0,0 +1,14 @@
+package edu.hw7;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Task1 {
+    static AtomicInteger counter = new AtomicInteger(0);
+
+    public static void incrementCounter() {
+        counter.incrementAndGet();
+    }
+    public static void clearCounter(){
+        counter.set(0);
+    }
+}
