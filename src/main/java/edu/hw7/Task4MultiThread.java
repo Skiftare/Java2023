04b0be3a7ac5e61d8f5a8ba1806a010bed1b3e3b
skiftare <74261401+Skiftare@users.noreply.github.com>
2023-11-25 19:12:16 +0500

    ускоряем

--- src/main/java/edu/hw7/Task4MultiThread.java
+++ src/main/java/edu/hw7/Task4MultiThread.java
@@ -1,11 +1,13 @@
 package edu.hw7;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Task4MultiThread implements Runnable {
-    private static final Object LOCK = new Object();
-    private static long totalCount = 0;
-    private static long circleCount = 0;
+    private static final AtomicLong totalCount = new AtomicLong(0);
+    private static final AtomicLong circleCount = new AtomicLong(0);
     private final long iterations;
     private static final double FOUR_VAL_FOR_MONTE_KARLO = 4.0;
     private static final double HALF_ONE_FOR_POINT_GENERATOR = 0.5;
@@ -17,31 +19,27 @@ public class Task4MultiThread implements Runnable {
     }
 
     public static double calcPi(int numThreads, long iterationsPerThread) throws InterruptedException {
-
-        Thread[] threads = new Thread[numThreads];
-
-        for (int i = 0; i < numThreads; i++) {
-            threads[i] = new Thread(new Task4MultiThread(iterationsPerThread));
-            threads[i].start();
-        }
+        ExecutorService executor = Executors.newFixedThreadPool(numThreads);
 
         for (int i = 0; i < numThreads; i++) {
-            threads[i].join();
+            executor.execute(new Task4MultiThread(iterationsPerThread));
         }
 
-        return FOUR_VAL_FOR_MONTE_KARLO * (circleCount / (double) totalCount);
+        executor.shutdown();
+        executor.awaitTermination(Long.MAX_VALUE, java.util.concurrent.TimeUnit.NANOSECONDS);
 
+        return FOUR_VAL_FOR_MONTE_KARLO * (circleCount.get() / (double) totalCount.get());
     }
 
     @Override
     public void run() {
-        //ThreadLocalRandom random = new ThreadLocalRandom();
         long localTotalCount = 0;
         long localCircleCount = 0;
+        ThreadLocalRandom random = ThreadLocalRandom.current();
 
         for (long i = 0; i < iterations; i++) {
-            double x = ThreadLocalRandom.current().nextDouble();
-            double y = ThreadLocalRandom.current().nextDouble();
+            double x = random.nextDouble();
+            double y = random.nextDouble();
             double radius = Math.pow(x - HALF_ONE_FOR_POINT_GENERATOR, TWO_AS_DEGREE_OF_POW_FOR_CIRCLE_FORMULA)
                 + Math.pow(y - HALF_ONE_FOR_POINT_GENERATOR, TWO_AS_DEGREE_OF_POW_FOR_CIRCLE_FORMULA);
             if (radius <= QUARTER_ONE_FOR_RADIUS_IN_GENERATOR) {
@@ -51,9 +49,7 @@ public class Task4MultiThread implements Runnable {
             localTotalCount++;
         }
 
-        synchronized (LOCK) {
-            totalCount += localTotalCount;
-            circleCount += localCircleCount;
-        }
+        totalCount.addAndGet(localTotalCount);
+        circleCount.addAndGet(localCircleCount);
     }
 }
