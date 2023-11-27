f7c8cd83d6558fe6e3128dada6c308a91947b6ce
skiftare <74261401+Skiftare@users.noreply.github.com>
2023-11-27 22:31:04 +0500

    Потихоньку допиливаю тесты

---
+++ src/main/java/edu/project3/utility/UtilityTableClass.java
@@ -0,0 +1,9 @@
+package edu.project3.utility;
+
+public class UtilityTableClass {
+    public static final String TOGGLE_TABLE_MODE_FOR_ADOC = "|===";
+    public static final Character ENDL_CHAR = '\n';
+    public static final Character CELL_SEPARATOR = '|';
+    public static final String TABLE_NAME_DETERMINANT_FOR_MARKDOWN = "# ";
+    public static final String HEADER_AND_BODY_SEPARATOR_FOR_MARKDOWN = "|---";
+}
