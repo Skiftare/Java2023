3174a072f66fa32790c5c61a06df77f8bf336c53
skiftare <74261401+Skiftare@users.noreply.github.com>
2023-11-28 19:52:29 +0500

    А почему тесты падают даже на парсинге сайта

--- src/test/java/edu/project3/TestContentOfReportInLogAnalyzer.java
+++ src/test/java/edu/project3/TestContentOfReportInLogAnalyzer.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class TestContentOfReportInLogAnalyzer {
-    <T,K> void thenAssertThatElementInMapExactlyExpectedTimes(T element, K expectedCount, Map<T,K> map){
+    <K,V> void thenAssertThatElementInMapExactlyExpectedTimes(K element, V expectedCount, Map<K,V> map){
         assertEquals(expectedCount,map.get(element));
     }
     @BeforeEach
@@ -27,75 +27,69 @@ public class TestContentOfReportInLogAnalyzer {
         LogAnalyzer.main(args);
 
         //then: check for some basic content in maps
-        Map<String, Integer> httpUserAgentMap = DataClass.getHttpUserAgentMap();
-        Map<String,Integer> httpReferMap = DataClass.getHttpRefererMap();
-        Map<Integer, Integer> statusMap = DataClass.getStatusMap();
 
         // Проверка основного содержимого карты HTTP_USER_AGENT_MAP
         thenAssertThatElementInMapExactlyExpectedTimes(
             "Debian APT-HTTP/1.3 (1.0.1ubuntu2)",
             11830,
-            httpUserAgentMap
+            DataClass.getHttpUserAgentMap()
         );
         thenAssertThatElementInMapExactlyExpectedTimes(
             "Debian APT-HTTP/1.3 (0.9.7.9)",
             11365,
-            httpUserAgentMap
+            DataClass.getHttpUserAgentMap()
         );
 
         //Проверка ReferMap
         thenAssertThatElementInMapExactlyExpectedTimes(
             "http://www.elasticsearch.org/overview/elkdownloads/",
         6,
-            httpReferMap
+            DataClass.getHttpRefererMap()
         );
 
         //Проверка statusMap
         thenAssertThatElementInMapExactlyExpectedTimes(
             304,
             13330,
-            statusMap
+            DataClass.getStatusMap()
         );
     }
 
     @Test
     @DisplayName("Парсинг с чётко данного пути в системе")
     public void testThatGetPathToOneFileAndReturnsContentOfMetric() {
-        //given: http-server & format
-        String[] args = {"--path", "src/test/java/edu/project3/resources/log1.txt", "--format", "markdown"};
+        //given: full path
+        String[] args = {"--path", "src/test/java/edu/project3/resources/log1.txt", "--format", "adoc"};
 
         //when: parse all logs
         LogAnalyzer.main(args);
 
         //then: check for some basic content in maps
-        Map<String, Integer> httpUserAgentMap = DataClass.getHttpUserAgentMap();
-        Map<String,Integer> httpReferMap = DataClass.getHttpRefererMap();
-        Map<Integer, Integer> statusMap = DataClass.getStatusMap();
 
         // Проверка основного содержимого карты HTTP_USER_AGENT_MAP
         thenAssertThatElementInMapExactlyExpectedTimes(
             "Debian APT-HTTP/1.3 (1.0.1ubuntu2)",
             11830,
-            httpUserAgentMap
+            DataClass.getHttpUserAgentMap()
         );
         thenAssertThatElementInMapExactlyExpectedTimes(
             "Debian APT-HTTP/1.3 (0.9.7.9)",
             11365,
-            httpUserAgentMap
+            DataClass.getHttpUserAgentMap()
         );
 
         //Проверка ReferMap
         thenAssertThatElementInMapExactlyExpectedTimes(
             "http://www.elasticsearch.org/overview/elkdownloads/",
             6,
-            httpReferMap
+            DataClass.getHttpRefererMap()
         );
 
         //Проверка statusMap
         thenAssertThatElementInMapExactlyExpectedTimes(
             304,
             13334,
-            statusMap
+            DataClass.getStatusMap()
         );
     }
 }
